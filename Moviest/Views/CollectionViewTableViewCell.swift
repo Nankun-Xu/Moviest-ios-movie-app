a47cdc8b20b4a9321d4256ff32c2fc12484ef4f2
Nankun-Xu <113734143+Nankun-Xu@users.noreply.github.com>
2024-03-16 23:25:12 -0700

    Creat herosection for homepage

--- Moviest/Views/CollectionViewTableViewCell.swift
+++ Moviest/Views/CollectionViewTableViewCell.swift
@@ -7,16 +7,53 @@
 
 import UIKit
 
+//horizontal rolling cells
 class CollectionViewTableViewCell: UITableViewCell {
 
     static let identifier = "CollectionViewTableViewCell"
     
+    private let collectionView:UICollectionView = {
+        let layout = UICollectionViewFlowLayout();
+        //Size of each cell
+        layout.itemSize = CGSize(width: 140, height: 200)
+        layout.scrollDirection = .horizontal
+        let collecttionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        collecttionView.register(UICollectionViewCell.self, forCellWithReuseIdentifier: "cell")
+        return collecttionView
+    }()
+    
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super .init(style: style, reuseIdentifier: reuseIdentifier)
         contentView.backgroundColor = .systemPink
+        contentView.addSubview(collectionView)
+        
+        collectionView.delegate = self
+        collectionView.dataSource = self
     }
     
     required init?(coder: NSCoder) {
         fatalError()
     }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        collectionView.frame = contentView.bounds
+    }
+    
+    
+}
+
+//num, color of cells in each cellcollection
+extension CollectionViewTableViewCell: UICollectionViewDelegate, UICollectionViewDataSource{
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath)
+        cell.backgroundColor = .blue
+        return cell
+    }
+    
+    
 }
