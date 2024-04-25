77a0bd191fdd0615a3ecf2568a3da79242c6724a
Nankun-Xu <113734143+Nankun-Xu@users.noreply.github.com>
2024-04-24 17:00:57 -0700

    Enable tap function in all pages

--- Moviest/Controllers/Core/HomeViewController.swift
+++ Moviest/Controllers/Core/HomeViewController.swift
@@ -17,6 +17,9 @@ enum Sections: Int {
 
 class HomeViewController: UIViewController {
     
+    private var randomTrendingMovie: Title?
+    private var headerView: HeroHeaderUIView?
+    
     let sectionTitles: [String] = ["Treding Movies", "Treding TV", "Popular", "Upcoming Movies", "Top Rated"]
     
     //tableview inside homescreen
@@ -38,11 +41,26 @@ class HomeViewController: UIViewController {
         configureNavbar()
         
         //headhero section
-        let headerView = HeroHeaderUIView(frame:CGRect(x: 0, y: 0, width: view.bounds.width, height: 450))
+        headerView = HeroHeaderUIView(frame:CGRect(x: 0, y: 0, width: view.bounds.width, height: 450))
         homeFeedTable.tableHeaderView = headerView
+        configureHeroHeaderView()
 
-            
-
+    }
+    
+    private func configureHeroHeaderView() {
+        //get random trending movie title
+        APICaller.shared.getTrendingMovies { [weak self] result in
+            switch result {
+            case .success(let titles):
+                let selectedTitle = titles.randomElement()
+                
+                self?.randomTrendingMovie = selectedTitle
+                self?.headerView?.configure(with: TitleViewModel(titleName: selectedTitle?.original_title ?? "", posterURL: selectedTitle?.poster_path ?? ""))
+                
+            case .failure(let erorr):
+                print(erorr.localizedDescription)
+            }
+        }
     }
     
     private func configureNavbar(){
