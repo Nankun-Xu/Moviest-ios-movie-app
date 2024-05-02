c9cc038c42c98db61fff74d19db731d3bcdcb493
Nankun-Xu <113734143+Nankun-Xu@users.noreply.github.com>
2024-05-01 17:31:20 -0700

    Download function and core data

---
+++ Moviest/Managers/PersistanceDataManager.swift
@@ -0,0 +1,20 @@
+//
+//  PersistanceDataManager.swift
+//  Moviest
+//
+//  Created by Nankun Xu on 5/1/24.
+//
+
+import Foundation
+import CoreData
+
+
+
+
+class DownloadManager {
+    static let shared = DownloadManager()
+
+    func downloadItem(with model: Title) {
+
+    }
+}
